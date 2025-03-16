eaa1662c29e208727fbe2d11514e1263bb7c824e
Nicholas Trimble <Ntrimb10@Wgu.edu>
2025-03-16 12:03:38 -0500

    finished setting up the project and generating API service using Http calls.

--- world-map-project/src/app/services/country.service.ts
+++ world-map-project/src/app/services/country.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
+
   private apiUrl = 'https://api.worldbank.org/v2/country/';
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  getCountryData(countryCode: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}${countryCode}?format=json`);
+  getCountryData(countryCode: string): Promise<any> {
+    return fetch(`${this.apiUrl}${countryCode}?format=json`)
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Error fetching country data:', error);
+        return []; 
+      });
   }
 }
