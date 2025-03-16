6c88db48321ca51b59c95b33ac84e4d0f52bcf42
Nicholas Trimble <Ntrimb10@Wgu.edu>
2025-03-15 23:36:45 -0500

    Configured Angular routing to direct to my map file

--- world-map-project/src/app/app.routes.ts
+++ world-map-project/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WorldMapComponent } from './world-map/world-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: 'map', component: WorldMapComponent }
+];
